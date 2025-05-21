c7844a2e1f08f80e69416a0121fcaf71b945b4ea
Matías Lopez <mts2311@hotmail.com>
2025-05-21 13:25:37 -0300

    tp n°5.1 funciones sin comentarios

--- 05 Funciones/lopezVicenteMatias_TP-05-funciones.py
+++ 05 Funciones/lopezVicenteMatias_TP-05-funciones.py
@@ -5,7 +5,7 @@ programa principal. """
 def imprimir_hola_mundo():
     return "Hola Mundo!"
 #Programa Principal
-""" print(imprimir_hola_mundo()) """
+print(imprimir_hola_mundo())
 
 """ 2. Crear una función llamada saludar_usuario(nombre) que reciba
 como parámetro un nombre y devuelva un saludo personalizado.
@@ -16,8 +16,8 @@ principal solicitando el nombre al usuario. """
 def saludar_usuario(nombre):
     return f"Hola {nombre}"
 #Programa Principal
-""" nombre = input("Ingresa tu nombre: ")
-print(saludar_usuario(nombre)) """
+nombre = input("Ingresa tu nombre: ")
+print(saludar_usuario(nombre))
 
 """ 3. Crear una función llamada informacion_personal(nombre, apellido,
 edad, residencia) que reciba cuatro parámetros e imprima: “Soy
@@ -27,11 +27,11 @@ los datos al usuario y llamar a esta función con los valores ingresados. """
 def informacion_personal(nombre, apellido, edad, residencia):
     return f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}"
 #Programa Principal
-""" nombre = input("Ingrese su nombre: ")
+nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 edad = input("Ingrese su edad: ")
 residencia = input("ingrese su residencia: ")
-print(informacion_personal(nombre, apellido, edad, residencia)) """
+print(informacion_personal(nombre, apellido, edad, residencia))
 
 """ 4. Crear dos funciones: calcular_area_circulo(radio) que reciba el radio
 como parámetro y devuelva el área del círculo. calcular_perimetro_
@@ -47,9 +47,9 @@ def calcular_perimetro_circulo(radio):
     perimetro = 2 * math.pi * radio
     return perimetro
 #Programa principal
-""" radio = float(input("Ingrese el radio del círculo: "))
+radio = float(input("Ingrese el radio del círculo: "))
 print(f"El área de este círculo es: {calcular_area_circulo(radio)}")
-print(f"El área de este círculo es: {calcular_perimetro_circulo(radio)}") """
+print(f"El área de este círculo es: {calcular_perimetro_circulo(radio)}")
 
 """ 5. Crear una función llamada segundos_a_horas(segundos) que reciba
 una cantidad de segundos como parámetro y devuelva la cantidad
@@ -60,8 +60,8 @@ def segundos_a_horas(segundos):
     horas = (segundos / 60) / 60
     return int(horas)
 #Programa principal
-""" segundos = int(input("Ingrese la cantidad de segundos: "))
-print(segundos_a_horas(segundos)) """
+segundos = int(input("Ingrese la cantidad de segundos: "))
+print(segundos_a_horas(segundos))
 
 """ 6. Crear una función llamada tabla_multiplicar(numero) que reciba un
 número como parámetro y imprima la tabla de multiplicar de ese
@@ -71,8 +71,8 @@ def tabla_multiplicar(numero):
     for i in range(1, 11):
         print(numero * i)
 #Programa principal
-""" num = int(input("Ingrese un número: "))
-tabla_multiplicar(num) """
+num = int(input("Ingrese un número: "))
+tabla_multiplicar(num)
 
 """ 7. Crear una función llamada operaciones_basicas(a, b) que reciba
 dos números como parámetros y devuelva una tupla con el resultado
@@ -87,12 +87,12 @@ def operaciones_basicas(a, b):
     resultados = [suma, resta, multiplicacion, division]
     return resultados
 #Programa principal
-""" num1 = int(input("Ingrese el primer número: "))
+num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 print(f"Suma: {operaciones_basicas(num1, num2)[0]}")
 print(f"Resta: {operaciones_basicas(num1, num2)[1]}")
 print(f"Multiplicación: {operaciones_basicas(num1, num2)[2]}")
-print(f"División: {operaciones_basicas(num1, num2)[3]}") """
+print(f"División: {operaciones_basicas(num1, num2)[3]}")
 
 """ 8. Crear una función llamada calcular_imc(peso, altura) que reciba el
 peso en kilogramos y la altura en metros, y devuelva el índice de
@@ -103,9 +103,9 @@ def calular_imc(peso, altura):
     imc = peso/(altura**2)
     return round(imc, 2)
 #Programa principal
-""" peso = float(input("Ingrese su peso: "))
+peso = float(input("Ingrese su peso: "))
 altura = float(input("Ingrese su altura: "))
-print(f"Su IMC es de: {calular_imc(peso, altura)}") """
+print(f"Su IMC es de: {calular_imc(peso, altura)}")
 
 """ 9. Crear una función llamada celsius_a_fahrenheit(celsius) que reciba
 una temperatura en grados Celsius y devuelva su equivalente en
@@ -116,8 +116,8 @@ def celsius_a_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 #Programa principal
-""" celsius = float(input("Ingrese los grados a calcular: "))
-print(f"{celsius}° celsius a fahrenheit es: {celsius_a_fahrenheit(celsius)}°.") """
+celsius = float(input("Ingrese los grados a calcular: "))
+print(f"{celsius}° celsius a fahrenheit es: {celsius_a_fahrenheit(celsius)}°.")
 
 """ 10.Crear una función llamada calcular_promedio(a, b, c) que reciba
 tres números como parámetros y devuelva el promedio de ellos.
